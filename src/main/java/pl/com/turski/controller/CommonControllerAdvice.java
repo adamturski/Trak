4de4b5f7edaebe4c0ceeedaed23a852e659f633b
Adam <adam.turski1@gmail.com>
2014-11-10 13:05:27 +0100

    Error page added. Handling BusinessException and redirecting to erro page after exception thrown.

--- src/main/java/pl/com/turski/controller/CommonControllerAdvice.java
+++ src/main/java/pl/com/turski/controller/CommonControllerAdvice.java
@@ -1,8 +1,14 @@
 package pl.com.turski.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
 import pl.com.turski.editor.ShipmentStatusEnumEditor;
+import pl.com.turski.exception.BusinessException;
 import pl.com.turski.model.domain.shipment.ShipmentStatusEnum;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +16,25 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * User: Adam
  */
-//@ControllerAdvice
+@ControllerAdvice
 public class CommonControllerAdvice {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CommonControllerAdvice.class);
+
     @InitBinder
     public void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) {
         binder.registerCustomEditor(ShipmentStatusEnum.class, new ShipmentStatusEnumEditor());
 
+    }
+
+
+    @ExceptionHandler(BusinessException.class)
+    public ModelAndView handleBusinessException(HttpServletRequest req, BusinessException exception) {
+        LOG.error("Business exception occurred", exception);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("message", exception.getErrorCode().name());
+        mav.setViewName("error");
+        return mav;
     }
 }
---
+++ src/main/webapp/WEB-INF/pages/error.jsp
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<div id="content">
+    <h1>Wystąpił błąd</h1>
+</div>
